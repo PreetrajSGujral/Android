14af5e397214001d2819864edb3d472ec6eb3179
RUGujral <44151163+RUGujral@users.noreply.github.com>
2018-11-23 16:53:41 -0500

    Create MainActivity.java

---
+++ TouchGesture/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.android.touch;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new DrawingView(this));
+    }
+
+    class DrawingView extends SurfaceView {
+
+        private final SurfaceHolder surfaceHolder;
+        private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        public DrawingView(Context context) {
+            super(context);
+            surfaceHolder = getHolder();
+            paint.setColor(Color.RED);
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                if (surfaceHolder.getSurface().isValid()) {
+                    Canvas canvas = surfaceHolder.lockCanvas();
+                    canvas.drawColor(Color.BLACK);
+                    canvas.drawCircle(event.getX(), event.getY(), 50, paint);
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+            }
+            return false;
+        }
+    }
+}
