0151f48bb9d820c65b4cd45921c1172c690aac34
RUGujral <44151163+RUGujral@users.noreply.github.com>
2018-11-24 21:31:54 -0500

    Update MainActivity.java

--- TouchGesture/MainActivity.java
+++ TouchGesture/MainActivity.java
@@ -1,66 +1,34 @@
-package com.example.android.touch;
+package com.example.android.assignment4touch;
 
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
-import android.text.StaticLayout;
-import android.text.TextPaint;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private float mPreviousX=0;
-    private float mPreviousY=0;
+    Button touch;
+    Button draw;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new DrawingView(this));
-    }
-
-    class DrawingView extends SurfaceView {
-
-        private final SurfaceHolder surfaceHolder;
-        private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
-        public DrawingView(Context context) {
-            super(context);
-            surfaceHolder = getHolder();
-            paint.setColor(Color.RED);
-        }
-
-        @Override
-        public boolean onTouchEvent(MotionEvent e) {
-            // MotionEvent reports input details from the touch screen
-            // and other input controls. In this case, you are only
-            // interested in events where the touch position changed.
-
-            float x = e.getX();
-            float y = e.getY();
-            if (e.getAction() == MotionEvent.ACTION_MOVE) {
-                float dx = x - mPreviousX;
-                float dy = y - mPreviousY;
-
-                if (surfaceHolder.getSurface().isValid()) {
-                    Canvas canvas = surfaceHolder.lockCanvas();
-                    canvas.drawColor(Color.BLACK);
-                    canvas.drawCircle(dx, dy, 50, paint);
-                    paint.setTextSize(25);
-                    canvas.drawText("Current X: " + dx + "  Current Y: " + dy + " Pointer count " + e.getPointerId(0), 40, 50, paint);
-                    surfaceHolder.unlockCanvasAndPost(canvas);
-
-                }
-
+        setContentView(R.layout.activity_main);
+        touch=(Button)findViewById(R.id.touch1);
+        draw=(Button)findViewById(R.id.draw1);
+        touch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TouchActivity.class);
+                startActivity(intent);
             }
-            return true;
-        }
+        });
+        draw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MainActivity.this, WriteOnScreenActivity.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
